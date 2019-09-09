802c8994b6a3a248ab046ee883002f88fd5c608c
Eric Joensson <erijo137@student.liu.se>
2019-09-09 13:13:56 +0200

    logic finished, todo: css

--- lab1/scripts/main.js
+++ lab1/scripts/main.js
@@ -1,5 +1,8 @@
+var i = 0;
+
 function sendMessage(form){
   var message = form.inputbox.value;
+  console.log(message.length);
   if (message.length > 140 || message.length < 1){
     console.log("Failure, you typed:  " + message);
     console.log(message.length);
@@ -7,8 +10,31 @@ function sendMessage(form){
   else{
     var parentnode = document.getElementById("messagelist");
     var melement = document.createElement('div');
-    melement.id = "message"
-    melement.innerHTML = "<h1>" + message + "</h1>" + '<input type="checkbox" name="checkbox" >'
+    melement.setAttribute('id', 'message' + i);
+    melement.setAttribute('class', 'unchecked');
+
+    var chbx = document.createElement('input');
+    chbx.setAttribute('type', 'checkbox');
+    chbx.setAttribute('id', 'checkbox' + i);
+    chbx.setAttribute('value', message);
+    chbx.onclick = toggleCheck
+    
+    var textbox = document.createElement('p');
+    textbox.appendChild(document.createTextNode(message));
+    melement.appendChild(textbox);
+    melement.appendChild(chbx);
+    
     parentnode.insertBefore(melement, parentnode.children[0]);
+    i += 1;
   }
 }
+
+function toggleCheck(){
+  if (this.checked){
+    this.parentElement.setAttribute('class', 'checked');
+  }
+  else{
+    this.parentElement.setAttribute('class', 'unchecked');
+  }
+}
+
