723e6ab59294dc93e4f4b02bcd0c619b479cd7ee
Eric Joensson <erijo137@student.liu.se>
2019-09-30 09:59:14 +0200

    todo: jquery requests

---
+++ lab3/client.js
@@ -0,0 +1,57 @@
+
+
+function sendMessage(message){
+  console.log(message);
+  var url = "http//:localhost:3000/save?message=" + message;
+  
+}
+/*
+function sendMessage(form){
+  var message = form.inputbox.value;
+
+  console.log(message.length);
+  
+  if (message.length > 140 || message.length < 1){
+    var error = document.createElement('p');
+    error.setAttribute('id', 'errormsg');
+    error.appendChild(document.createTextNode('Error: Too many or too few characters. Max 140.'));
+
+    var errbtn = document.createElement('input');
+    errbtn.setAttribute('type', 'button');
+    errbtn.setAttribute('id', 'errbtn');
+    errbtn.setAttribute('value', 'OK');
+    errbtn.onclick = closeError;
+    error.appendChild(errbtn);
+    document.body.appendChild(error);
+
+  }
+  else{
+    var parentnode = document.getElementById("messagelist");
+
+    var chbx = document.createElement('input');
+    chbx.setAttribute('type', 'checkbox');
+    chbx.setAttribute('class', 'checkbox');
+    chbx.onclick = toggleCheck;
+    
+    var textbox = document.createElement('p');
+    textbox.setAttribute('class', 'unchecked');
+    textbox.appendChild(document.createTextNode(message));
+    textbox.appendChild(chbx);
+    
+    parentnode.insertBefore(textbox, parentnode.children[0]);
+  }
+}
+
+function toggleCheck(){
+  if (this.checked){
+    this.parentElement.setAttribute('class', 'checked');
+  }
+  else{
+    this.parentElement.setAttribute('class', 'unchecked');
+  }
+}
+
+function closeError(){
+  document.getElementById('errormsg').remove();
+}
+*/
