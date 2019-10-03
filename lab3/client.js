bde2efef396418a1f7e4b742e4ab3aada1238862
Eric Joensson <erijo137@student.liu.se>
2019-10-03 14:56:24 +0200

    added most of the functionality, needs testing and ajusting

--- lab3/client.js
+++ lab3/client.js
@@ -5,83 +5,76 @@ function sendMessage(message){
 	$.get(url, function(data, status){
 		console.log(data);
 		console.log(status);
+		displayMessages();
 	}).fail( 
 	function(res){
 		console.log(res["responseText"]);
 		var data = res["responseText"];
-
-		var error = document.createElement('p');
-		error.setAttribute('id', 'errormsg');
-		error.appendChild(document.createTextNode(data));
-
-		var errbtn = document.createElement('input');
-		errbtn.setAttribute('type', 'button');
-		errbtn.setAttribute('id', 'errbtn');
-		errbtn.setAttribute('value', 'OK');
-		errbtn.onclick = closeError;
-		error.appendChild(errbtn);
-		document.body.appendChild(error);
+		sendError(data);
 	});	
 }
-/*
-$(document).ready(function(){
-	$("#sendbutton").click(function(){
-		var url = "http//:localhost:3000/save?message=" + $('form[name="message"]');
-		console.log(url);
-		console.log(typeof($("#inputbox")));
-	  $.get(url, function(data, status){
 
-    });
-	});
-});
+function displayMessages(){
+	var url = "http://localhost:3000/getall"; 
+	var parentnode = document.getElementById("messagelist");
+	parentnode.innerHTML = "";
 
-/*
-function sendMessage(form){
-	var message = form.inputbox.value;
 
-	console.log(message.length);
-	
-	if (message.length > 140 || message.length < 1){
-		var error = document.createElement('p');
-		error.setAttribute('id', 'errormsg');
-		error.appendChild(document.createTextNode('Error: Too many or too few characters. Max 140.'));
-
-		var errbtn = document.createElement('input');
-		errbtn.setAttribute('type', 'button');
-		errbtn.setAttribute('id', 'errbtn');
-		errbtn.setAttribute('value', 'OK');
-		errbtn.onclick = closeError;
-		error.appendChild(errbtn);
-		document.body.appendChild(error);
-
-	}
-	else{
-		var parentnode = document.getElementById("messagelist");
-
-		var chbx = document.createElement('input');
-		chbx.setAttribute('type', 'checkbox');
-		chbx.setAttribute('class', 'checkbox');
-		chbx.onclick = toggleCheck;
-		
-		var textbox = document.createElement('p');
-		textbox.setAttribute('class', 'unchecked');
-		textbox.appendChild(document.createTextNode(message));
-		textbox.appendChild(chbx);
-		
-		parentnode.insertBefore(textbox, parentnode.children[0]);
-	}
+	$.get(url, function(data, status){
+		console.log(data);
+		console.log(status);
+		for(message in data){
+			console.log(data[message]["message"]);
+			var id = data[message]["_id"];
+			var textbox = document.createElement('p');
+			textbox.setAttribute('id', id);
+			textbox.onclick = function(){
+				setRead(this.id);
+			}
+			if (data[message]["flag"] == true){
+				textbox.setAttribute('class', 'read');
+			}else{
+				textbox.setAttribute('class', 'unread');
+			}
+			textbox.appendChild(document.createTextNode(data[message]["message"]));
+			parentnode.insertBefore(textbox, parentnode.children[0]);
+		}
+	});	
 }
 
-function toggleCheck(){
-	if (this.checked){
-		this.parentElement.setAttribute('class', 'checked');
-	}
-	else{
-		this.parentElement.setAttribute('class', 'unchecked');
-	}
+function sendError(message){
+	var error = document.createElement('p');
+	error.setAttribute('id', 'errormsg');
+	error.appendChild(document.createTextNode(message));
+
+	var errbtn = document.createElement('input');
+	errbtn.setAttribute('type', 'button');
+	errbtn.setAttribute('id', 'errbtn');
+	errbtn.setAttribute('value', 'OK');
+	errbtn.onclick = closeError;
+	error.appendChild(errbtn);
+	document.body.appendChild(error);
+
+ }
+
+function setRead(id){
+	var url = "http://localhost:3000/flag?id=" + id;
+	$.get(url, function(data, status){
+		console.log(data);
+		console.log(status);
+		document.getElementById(id).setAttribute('class', 'read');
+	}).fail( 
+	function(res){
+		console.log(res["responseText"]);
+		var data = res["responseText"];
+		sendError(data);
+
+	});	
 }
-*/
+
 
 function closeError(){
 	document.getElementById('errormsg').remove();
 }
+
+displayMessages();
